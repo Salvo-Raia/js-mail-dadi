2c6facfc1f7ffca3e3d8f37cae43f35ba64d3936
Salvo-Raia <salvo.raia@outlook.it>
2025-12-20 18:50:41 +0100

    Add match system

--- j02. js-dadi/script.js
+++ j02. js-dadi/script.js
@@ -1,13 +1,13 @@
 
 //* Queste due variabili fanno parte del Bonus autoassegnato
-let userPoints = 0;
-let pcPoints = 0;
+let userWonMatches = 0;
+let pcWonMatches = 0;
 
 //TODO Bonus Tutor
 let gameStop = false;
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-for (i = 0; gameStop !== true; i++) {
+for (i = 0;  i < 5 && gameStop !== true ; i++) {
 console.log("Tira un dado!");
 let userDice = (prompt("Digita un numero da 1 a 6"));
 if (userDice.toUpperCase() == "STOP") {
@@ -20,25 +20,27 @@ let pcDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
 console.log("Il PC ha fatto:", pcDice);
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (userDice > pcDice) {
-    userPoints += 1; 
+    userWonMatches += 1; 
     console.log("%cHai vinto", "background: black; color: green;");
 } else if (userDice == pcDice) {
     console.log("%cPareggio!", "background: black; color: yellow;");
     } else {
-    pcPoints += 1;
+    pcWonMatches += 1;
     console.log("%cHai perso!", "background: black; color: red;");
 }
 }
 }
 
-
-// TODO Funzionava come volevo prima del bonus Tutor, ora è da rivedere
-
-/* console.log("Tu hai totalizzato:", userPoints,". Il PC ha realizzato:", pcPoints);
-if (userPoints > pcPoints) {
-    console.log("%cCongratulazioni! Hai vinto", "background: black; color: green;");  
-} else if (userPoints == pcPoints) {
-    console.log("%cE' finita in pareggio! Aggiorna la pagina se vuoi una rivincita", "background: black; color: yellow;");
+//* Bonus autoassegnato
+if (gameStop === false) { 
+    console.log("Hai vinto:", userWonMatches,"match! Il PC invece:", pcWonMatches);
+    if (userWonMatches> pcWonMatches) {
+        console.log("%cCongratulazioni! Hai vinto la partita", "background: black; color: green;");  
+    } else if ( userWonMatches == pcWonMatches) {
+        console.log("%cE' finita in pareggio! Aggiorna la pagina se vuoi una rivincita", "background: black; color: yellow;");
+    } else {
+        console.log("%cOh no, Hai perso!", "background: black; color: red;")
+    } 
 } else {
-    console.log("%cOh no, hai perso!", "background: black; color: red;")
-} */
+    console.log("Hai abbandonato la partita prima della fine, il punteggio non viene calcolato");
+}
